107ddcdef4f6986eb9fa665c41231a01df013794
Lea <leandrogabrielgarcia@gmail.com>
2017-11-14 17:20:10 -0300

    Creando diccionario y agregando ruta para agregar

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-'use strict';
-var express = require("express");
-var parser = require("body-parser");
+'use strict'
+var express = require('express');
+var parser = require('body-parser');
 var args = require('margs').setDefault({maxmem: 100});
 var app = express();
 
-const http = require('http');
+app.use(parser.urlencoded({extended:false}));
+app.use(parser.json());
+
 var port = args.get('port');
 
 if (port === undefined) {
     console.log('nodejs server.js -port=<PORT>');
     process.exit();
-}
+};
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Hello Node.js Server!');
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
+app.listen(port, (err) => {
   if (err) {
 	  console.log('No levanto el puerto', err);
     return
   }
-
   console.log('server is listening on port '+ port);
+});
 
-})
+app.get('/', function(req,res){
+  res.send({title: "Diccionario distribuido"});
+});
+
+app.get('/prueba', function(req,res){
+  res.send({message: "asd"});
+});
+
+//Creating dictionary
+var dict = new Object();
+
+app.get('/load/:key/:value', function(req, res){
+  var key = req.params.key;
+  var value = req.params.value;
+  dict[key] = value;
+  console.log('Diccionario: ' + dict);
+  var responseObject = { message: 'Se agrego al diccionario, Key: '+ key +' '+'Value: '+ value };
+  res.send(responseObject);
+});

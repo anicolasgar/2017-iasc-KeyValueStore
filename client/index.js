c278c2850ff2435ea94e9366666214ef39d8ed6c
anicolasgar <a.nicolasgar@gmail.com>
2017-12-19 18:07:16 -0300

    Agrego funciones de delete, mayores y menores al cliente

--- client/index.js
+++ client/index.js
@@ -5,11 +5,13 @@ var request = require('request');
 var config = require('./config');
 var randomWords = require('random-words');
 
+require('events').EventEmitter.prototype._maxListeners = 100;
+
 
 config.orquestadores.forEach(function(){
     //conectarse al maestro
-  });
-  
+});
+
 
 var loadfunct = (clave,valor) => {
     request({ method: 'POST', url:'http://localhost:8084/load', form: {key:clave,value:valor}}, function(error, response,body) {
@@ -19,7 +21,7 @@ var loadfunct = (clave,valor) => {
         else if ( error ){
             console.log(error);
         }
-     
+
     });
 };
 
@@ -32,6 +34,46 @@ var getfunct = (clave) => {
             console.log(error);
         }
         console.log('El valor de "' + clave + '" es ' +response.body);
+        console.log("\n");
+    });
+};
+
+var deletefunct = (clave) => {
+    request.delete({ url:'http://localhost:8084/delete/' + clave}, function(error, response,body) {
+        if (response.statuscode == 200) {
+            console.log(response);
+        }
+        else if ( error ){
+            console.log(error);
+        }
+        console.log('El valor de "' + clave + '" fue eliminado: ' +response.body);
+        console.log("\n");
+    });
+};
+
+var mayoresfunct = (clave) => {
+    request.get({ url:'http://localhost:8084/mayores/' + clave}, function(error, response,body) {
+        if (response.statuscode == 200) {
+            console.log(response);
+        }
+        else if ( error ){
+            console.log(error);
+        }
+        console.log('Los valores mayores de:"' + clave + '" son ' +response.body);
+        console.log("\n");
+    });
+};
+
+var menoresfunct = (clave) => {
+    request.get({ url:'http://localhost:8084/menores/' + clave}, function(error, response,body) {
+        if (response.statuscode == 200) {
+            console.log(response);
+        }
+        else if ( error ){
+            console.log(error);
+        }
+        console.log('El valorres menores de "' + clave + '" son ' +response.body);
+        console.log("\n");
     });
 };
 
@@ -40,25 +82,43 @@ var listar = (clave) => {
 }
 
 program
-    .version('0.0.1')
-    .command('listar')
-    .description('List all ips and ports')
+.version('0.0.1')
+.command('listar')
+.description('List all ips and ports')
     //.option('-o', 'List ips and port from orquestadores')
     //.option('-d', 'List ips and port from datanodes')
     .action(listar);
     
-program
+    program
     .version('0.0.1')
     .command('load <clave> <valor>')
     .description('load key:value')
     .action(loadfunct);
     
-program
+    program
     .version('0.0.1')
     .command('get <clave>')
     .description('get key')
     .action(getfunct);
 
-program.parse(process.argv);
+    program
+    .version('0.0.1')
+    .command('delete <clave>')
+    .description('delete key')
+    .action(deletefunct);
 
-if (program.args.length === 0) program.help();
+     program
+    .version('0.0.1')
+    .command('menores <clave>')
+    .description('menores key')
+    .action(menoresfunct);
+
+     program
+    .version('0.0.1')
+    .command('mayores <clave>')
+    .description('mayores key')
+    .action(mayoresfunct);
+
+    program.parse(process.argv);
+
+    if (program.args.length === 0) program.help();
